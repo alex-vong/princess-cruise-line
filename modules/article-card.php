91f857a4494452a65b022a83bd64dc0ddba13c0f
Alex Vong <avong003@gmail.com>
2021-09-30 14:17:58 -0700

    modules

---
+++ modules/article-card.php
@@ -0,0 +1,20 @@
+
+<article-card>
+
+	<picture class='grid-image'>
+		<img src='https://peprojects.dev/images/landscape.jpg' loading='lazy' alt='$todo'>
+	</picture>
+
+	<section class="article-card-details">
+		<h2 class='attention-voice'><?=$article["heading"]?></h2>
+
+		<p class='calm-voice'><?=$article["description"]?></p>
+
+		<a href='#'>
+			<span>Read more</span>
+		</a>
+	</section>
+
+
+
+</article-card>
---
+++ modules/photo-grid.php
@@ -0,0 +1,24 @@
+<section class="photo-grid-container">
+	<picture class='img-1'>
+		<img src="https://d3qvqlc701gzhm.cloudfront.net/thumbs/5805f754e31c2f1af62a85123dc2935b2e49080f6ad5b4e2055287bf45f6bb9e-750.jpg" alt="girl-on-beach">
+	</picture>
+	<picture class='img-2'>
+
+		<img srcset="https://d3qvqlc701gzhm.cloudfront.net/thumbs/e3f0177c467f44af57bb9c14b14bf091764253c1eaf2b6e602edbe6262a5a420-750.jpg" alt="food-on-deck">
+	</picture>
+	<picture class='img-3'>
+		<img src="https://d3qvqlc701gzhm.cloudfront.net/thumbs/b1f6d489481515ee67ced0256ff1ac89cdce91e3b00c008663cb64e46fa35840-500.jpg" alt="cruise-water">
+	</picture>
+	<picture class='img-4'>
+		<img src="https://d3qvqlc701gzhm.cloudfront.net/thumbs/4ddeb4f619e01f86f8d6d69b53014e750bcca25a01e6fa741a473c3de329a831-750.jpg" alt="alaskan-cruise">
+	</picture>
+	<picture class='img-5'>
+		<img src="https://images.unsplash.com/photo-1599700548547-10a85af5b390?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGNydWlzZSUyMGxpbmV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60" alt="activities">
+	</picture>
+	<picture class='img-6'>
+		<img src="https://images.unsplash.com/photo-1563829467124-e97ae7f229fd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80" alt="skyliner">
+
+
+
+	</picture>
+</section>
