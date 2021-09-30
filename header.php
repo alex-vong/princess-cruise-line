40988b19741ac4bfa545d1bd3e9544555faa9449
Alex Vong <avong003@gmail.com>
2021-09-30 14:18:17 -0700

    header

---
+++ header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+
+<html class="no-js" lang="">
+	<?php include('head.php'); ?>
+
+	<body>
+		<header class='site-header'>
+		<inner-column>
+
+			<?php include('modules/masthead.php'); ?>
+
+		</inner-column>
+		</header>
+
+
+		<main class='page-content'>
+			<!-- STUFF -->
