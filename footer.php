77121fd7087df2409dffc4ce3ee92dc01b86b042
Alex Vong <avong003@gmail.com>
2021-09-30 14:18:03 -0700

    footer

---
+++ footer.php
@@ -0,0 +1,22 @@
+
+			<!-- STUFF -->
+		</main>
+
+
+		<footer>
+		<inner-column>
+			<div class="site-footer">
+				<!-- no headers in footers allowed? -->
+				<div class='what-to-call-it-then'>
+					<h2 class='attention-voice'>This is the site footer and it has a "site-map" module in it.</h2>
+
+					<p>Pretty standard type of thing, right?</p>
+				</div>
+
+				<?php include('modules/site-map.php'); ?>
+			</div>
+		</inner-column>
+		</footer>
+
+	</body>
+</html>
