827525e704ba18639a7e453b47425ca674c847aa
Alex Vong <avong003@gmail.com>
2021-10-01 20:28:30 -0700

    added alert bar php

---
+++ modules/alert-bar.php
@@ -0,0 +1,12 @@
+<alert-bar>
+
+	<section class="update-message">
+			<div class="update whisper-voice">Update</div>
+			<div class="close whisper-voice"><span>Close</span><i class="far fa-times-circle"></i></div>	
+	</section>
+
+	<section class="protocol whisper-voice">
+		<p>Stay up-to-date on <a href="#"> cancelled cruises</a>, <a href="#"> travel advisories</a>, and <a href="#"> health protocols</a>.</p>
+	</section>
+
+</alert-bar>
