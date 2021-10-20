fe6742691dd255530c28b5abf37706cd2df414d7
Alex Vong <avong003@gmail.com>
2021-10-19 23:48:20 -0700

    added confidence data

---
+++ modules/confidence-data.php
@@ -0,0 +1,21 @@
+<?php
+
+$confidenceData = [
+	[
+		"img" => "images/medallion-class.jpeg",
+		"heading" => "MedallionClass". "<span>&#174;</span>" . " On All Ships!",
+		"description" => "Next-level technology. More time for you. Your vacation - simplified, personalized.",
+	],
+	[
+		"img" => "images/cruise-with-confidence.jpeg",
+		"heading" => "More Flexibility & Less Worry",
+		"description" => "Learn about our health protocols, booking flexibility and peace of mind.",
+	],
+	[
+		"img" => "images/cruise-health.jpeg",
+		"heading" => "Your Safety if Our Priority",
+		"description" => "Learn what best practices we're following to protect the health of guest and crew.",
+	],
+];
+
+echo $confidenceData["img"];
